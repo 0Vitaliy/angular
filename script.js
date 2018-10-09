7027fbd82f47b2e24f87d53723eed2db0bdc322f
Makovskyi Vitaliy <vitalic10051998@ukr.net>
2018-10-09 19:52:13 +0300

    sd

--- script.js
+++ script.js
@@ -5,22 +5,18 @@ var App = angular.module('httpExample', []);
   
   App.controller('FetchController', ['$scope','$http' ,
     function($scope, $http) {
-      // $scope.method = 'GET';
-      // $scope.url = 'https://demo4070131.mockable.io/product';
+      $scope.method = 'GET';
+      $scope.url = 'https://demo4070131.mockable.io/product';
 
       $scope.fetch = function() {
         
-
-       $http({method: 'GET', url: 'https://demo4070131.mockable.io/product'}).
+    
+       $http({method: $scope.method, url: $scope.url}).
           then(function(response) {
             $scope.status = response.status;
             $scope.products = response.data.products;
-          }, function(response) {
-            $scope.products = response.data.products || 'Request failed';
-            $scope.status = response.status;
-        });
+          });
       
       };
     }]);
-
 
